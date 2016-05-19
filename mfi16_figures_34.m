f32c8a2cc5a2ae2affa82cc9df258fff833697cc
Alex Burka <aburka@seas.upenn.edu>
2016-05-19 00:12:12 -0400

    widen CV

--- mfi16_figures_34.m
+++ mfi16_figures_34.m
@@ -1,12 +1,12 @@
 % part 34 of /Users/alex/Documents/research/proton/code/calibration/motion/mfi16_figures.m
 %
 % hyperparameters
-nbins = 5:5:30; % 20
+nbins = 1:2:20; % 20
 binmode = {'perceptual'}; % perceptual
-alpha = 0.05:0.05:0.5; % 25
-nu = .1:0.05:0.7; % .6
-gamma = [1 5 10 20]; % 200
-stmode = [false true]; % false
+alpha = 0.1:0.05:0.5; % 25
+nu = .05:0.05:0.7; % .6
+gamma = 1:2:20; % 200
+stmode = true; % false
 
 gs_limits = [length(nbins) length(binmode) length(alpha) length(nu) length(gamma) length(stmode)];
 gs_idx = repmat(ones(size(gs_limits)), prod(gs_limits), 1);

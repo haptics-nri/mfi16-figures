1d3bc486e853f5c51e4dafee5d083daebf2465f1
Alex Burka <aburka@seas.upenn.edu>
2016-07-04 19:02:51 -0400

    narrow GS ranges because faster

--- choose_stick_23.m
+++ choose_stick_23.m
@@ -2,11 +2,11 @@
 % cross-validation
 
 % hyperparameters
-nbins = 1:2:20; % 20
+nbins = 1:2:10; % 20
 binmode = {'perceptual'}; % perceptual
 alpha = 0.1:0.05:0.5; % 25
-nu = .05:0.05:0.7; % .6
-gamma = 1:2:20; % 200
+nu = .05:0.05:0.3; % .6
+gamma = 3:2:15; % 200
 stmode = true; % false
 
 gs_limits = [length(nbins) length(binmode) length(alpha) length(nu) length(gamma) length(stmode)];
@@ -15,7 +15,7 @@ for j=2:size(gs_idx,1)
     choose_stick_24
 end
 
-for i=1:4
+for i=1:2
     choose_stick_28
 end
 

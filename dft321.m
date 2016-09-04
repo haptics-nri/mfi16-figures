30d53c46a948887cc16ff84c5ae2c12b747c17b6
Alex Burka <aburka@seas.upenn.edu>
2016-09-04 17:48:17 -0400

    dft321: fix indexing

--- dft321.m
+++ dft321.m
@@ -7,15 +7,15 @@ function [out, freq] = dft321(in, filt)
     b = [1 .02-1];
 
     if nargin > 1 && filt
-        A = fft(filtfilt(b, a, in));
+        A = fft(filtfilt(b, a, in), 2*size(in,1));
     else
-        A = fft(bsxfun(@minus, in, mean(in)));
+        A = fft(bsxfun(@minus, in, mean(in)), 2*size(in,1));
     end
     A_s = sqrt(sum(A .* conj(A), 2));
     theta = angle(sum(A,2));
     
     freq = A_s .* exp(1j * theta);
     out = real(ifft(freq, size(in,1)));
-    freq = abs(freq(ceil(length(freq)/2):end));
+    freq = abs(freq(end-size(in,1)+2:end));
 
 end
